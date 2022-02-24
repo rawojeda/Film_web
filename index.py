027145fccee1ee178e046a3e3b3ddf8b33170024
rawojeda <raw_ojeda99@hotmail.com>
2022-02-24 11:53:56 +0100

    Mi primer commit

---
+++ index.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app= Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('Index.html')
+
+@app.route('/Films')
+def films_to_see():
+    films=("Avril y el mundo alterado", "Hair high", "kung fu hustle", "Neo tokyo",
+           "Redline", "last night in soho", "gozu", "The Congress", "House")
+    return render_template('Films.html',peliculas= films)
+
+@app.route('/Contact')
+def contacto():
+    return render_template('Contact.html')
+
+
+if __name__ ==  '__main__':
+    app.run(debug=True,port=5000)
